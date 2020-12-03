a10bc684bb1acf72077141b6e017ebd6db13f3cc
Crowdbotics <team@crowdbotics.com>
2020-12-03 08:19:48 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -8,8 +8,80 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(
+        blank=True,
         max_length=150,
     )
+    dgyryrturut = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    drgyeryeyrthyfh = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    ghjfhfhdgdgdg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    fgdfg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
     def __str__(self):
         return self.title
@@ -33,3 +105,52 @@ class HomePage(models.Model):
     @property
     def field(self):
         return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+
+class Test(models.Model):
+    "Generated Model"
+    jngjhgj = models.BigIntegerField()
+    cgvfbgbhnjnmj = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
