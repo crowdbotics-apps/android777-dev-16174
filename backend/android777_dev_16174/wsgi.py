8ebe07326eef37a5428606d590afd54b87ec7f62
Crowdbotics <team@crowdbotics.com>
2020-12-03 08:00:56 +0000

    Update models

--- backend/android777_dev_16174/wsgi.py
+++ backend/android777_dev_16174/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'android777_dev_16174.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "android777_dev_16174.settings")
 
 application = get_wsgi_application()
